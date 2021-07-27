25ed614cbbb5dd8f753b0eb0c72ae157b76f5417
Alexis Dumon <alexis.dumon@kcl.ac.uk>
2021-07-27 10:37:25 +0100

    changes layout of files and refactors the webcamdetect script

--- UnityKobe/Assets/Scripts/WebCamDetect.cs
+++ UnityKobe/Assets/Scripts/WebCamDetect.cs
@@ -8,29 +8,14 @@ public class WebCamDetect : MonoBehaviour
     void Start()
     {
           WebCamDevice[] devices = WebCamTexture.devices;
-          Debug.Log("Number of web cams connected: " + devices.Length);
-
-          for (int i = 0; i < devices.Length; i++) {
-            Debug.Log(i + " " + devices[i].name);
-          }
-
           Renderer rend = this.GetComponentInChildren<Renderer>();
 
           // hardcoding the device name
           WebCamTexture mycam = new WebCamTexture(devices[0].name);
 
-          Debug.Log(mycam.deviceName + " mycam");
-          // Debug.Log(mycam.deviceName + " deviceName");
-
-          // mycam.deviceName = camName;
-
-          // Debug.Log("poop");
-
           rend.material.mainTexture = mycam;
-
-          Debug.Log("LALALALA: " + rend.material.mainTexture);
           if (mycam == null) {
-            Debug.Log("fack");
+            Debug.Log("No device connected");
           } else {
             mycam.Play();
           }
