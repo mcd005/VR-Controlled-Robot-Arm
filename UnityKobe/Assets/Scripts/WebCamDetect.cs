98a4d3fddcb42aabfc02a2639399f92b814d0943
czar <czar.balangue@paconsulting.com>
2021-07-28 15:28:54 +0100

    sets camera inside sphere and video stream to display inside the sphere

--- UnityKobe/Assets/Scripts/WebCamDetect.cs
+++ UnityKobe/Assets/Scripts/WebCamDetect.cs
@@ -10,8 +10,11 @@ public class WebCamDetect : MonoBehaviour
           WebCamDevice[] devices = WebCamTexture.devices;
           Renderer rend = this.GetComponentInChildren<Renderer>();
 
+          for (int i = 0; i < devices.Length; i++){
+            Debug.Log(devices[i].name);
+          }
           // hardcoding the device name
-          WebCamTexture mycam = new WebCamTexture(devices[0].name);
+          WebCamTexture mycam = new WebCamTexture(devices[1].name);
 
           rend.material.mainTexture = mycam;
           if (mycam == null) {
