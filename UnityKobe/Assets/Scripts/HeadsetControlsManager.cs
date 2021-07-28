0bd0aacfe63216fc8894fb36574a812aa71fafb4
czar <czar.balangue@paconsulting.com>
2021-07-28 16:45:29 +0100

    changes a setting

---
+++ UnityKobe/Assets/Scripts/HeadsetControlsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// FOR NOW very basic - can I record the position of the right hand controller through the environment?
+public class HeadsetControlsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      var position = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+      Debug.Log("What ios the position?: " + position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
