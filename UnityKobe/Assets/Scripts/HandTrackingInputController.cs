3deef6205ee0d61ed389121643f24d0a710691b6
czar <czar.balangue@paconsulting.com>
2021-08-05 17:33:40 +0100

    intoduces a very annoying errow with TCPClient

---
+++ UnityKobe/Assets/Scripts/HandTrackingInputController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTrckingInputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // var hand = GetComponent<OVRHand>();
+      // bool isIndexFingerPinching = hand.GetFingerIsPinching(HandFinger.Index);
+      // float ringFingerPinchStrength = hand.GetFingerPinchStrength(HandFinger.Index);
+      // TrackingConfidence confidence = hand.GetFingerConfidence(HandFinger.Index);
+      //
+      // Debug.Log(isIndexFingerPinching);
+      // Debug.Log(ringFingerPinchStrength);
+    }
+}
