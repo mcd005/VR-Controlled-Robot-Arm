f689c9e18a705903a34f705f11d77696fa7222b0
Alexis Dumon <alexis.dumon@kcl.ac.uk>
2021-08-06 18:09:54 +0100

    sets up a http client on Unity side and server in python to send json data to any url

--- UnityKobe/Assets/Scripts/CommsManager.cs
+++ UnityKobe/Assets/Scripts/CommsManager.cs
@@ -1,174 +1,57 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using UnityEngine;
-
-// using System.Net.Sockets.dll;
+using UnityEngine.Networking;
+using System.Text;
 
 namespace CommsManagerSpace
 {
 
-    public class CommsManager
+    public class CommsManager : MonoBehaviour
     {
-        #region private members
-        /// <summary>
-        /// TCPListener to listen for incomming TCP connection
-        /// requests.
-        /// </summary>
-        private TcpListener tcpListener;
-        /// <summary>
-        /// Background thread for TcpServer workload.
-        /// </summary>
-        private Thread tcpListenerThread;
-        /// <summary>
-        /// Create handle to connected tcp client.
-        /// </summary>
-        private TcpClient connectedTcpClient;
-        #endregion
-
-        // Use this for initialization
-        void Start()
+        public string json;
+        private void Start()
         {
-            // Start TcpServer background thread
-            tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
-            tcpListenerThread.IsBackground = true;
-            tcpListenerThread.Start();
+            Debug.Log("lalal");
+
+            OperatorData data = new OperatorData();
+
+            data.RControllerPos = "(1, 2, 3)";
+            data.AButtonPressed = "False";
+
+            json = JsonUtility.ToJson(data);
+            Debug.Log(json);
+            StartCoroutine(SendData());
+
         }
 
-        // Update is called once per frame
-        void Update()
+        // adapted from: https://answers.unity.com/questions/1491938/sending-an-https-post-request-with-json-body.html
+        IEnumerator SendData()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            var request = new UnityWebRequest("http://localhost:5000/", "POST");
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            yield return request.SendWebRequest();
+
+            if (request.error != null)
             {
-                SendMessage();
+                Debug.Log("Error");
+                Debug.Log("Status Code: " + request.responseCode);
             }
+            else
+            {
+                Debug.Log("All OK");
+                Debug.Log("Status Code: " + request.responseCode);
+            }
+                         
         }
 
-        /// <summary>
-        /// Runs in background TcpServerThread; Handles incomming TcpClient requests
-        /// </summary>
-        private void ListenForIncommingRequests()
+        private class OperatorData
         {
-            try
-            {
-                // Create listener on localhost port 8052.
-                tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8052);
-                tcpListener.Start();
-                Debug.Log("Server is listening");
-                Byte[] bytes = new Byte[1024];
-                while (true)
-                {
-                    using (connectedTcpClient = tcpListener.AcceptTcpClient())
-                    {
-                        // Get a stream object for reading
-                        using (NetworkStream stream = connectedTcpClient.GetStream())
-                        {
-                            int length;
-                            // Read incomming stream into byte arrary.
-                            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
-                            {
-                                var incommingData = new byte[length];
-                                Array.Copy(bytes, 0, incommingData, 0, length);
-                                // Convert byte array to string message.
-                                string clientMessage = Encoding.ASCII.GetString(incommingData);
-                                Debug.Log("client message received as: " + clientMessage);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SocketException socketException)
-            {
-                Debug.Log("SocketException " + socketException.ToString());
-            }
+            public string RControllerPos;
+            public string AButtonPressed; 
         }
-        /// <summary>
-        /// Send message to client using socket connection.
-        /// </summary>
-        public void SendMessage()
-        {
-            if (connectedTcpClient == null)
-            {
-                return;
-            }
-
-            try
-            {
-                // Get a stream object for writing.
-                NetworkStream stream = connectedTcpClient.GetStream();
-                if (stream.CanWrite)
-                {
-                    string serverMessage = "This is a message from your server.";
-                    // Convert string message to byte array.
-                    byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
-                    // Write byte array to socketConnection stream.
-                    stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
-                    Debug.Log("Server sent his message - should be received by client");
-                }
-            }
-            catch (SocketException socketException)
-            {
-                Debug.Log("Socket exception: " + socketException);
-            }
-        }
-
-
-        // private static int localPort;
-        //
-        // // prefs
-        // private string IP;  // define in init
-        // public int port;  // define in init
-        //
-        // private UdpClient upd;
-        //
-        // public CommsManager() {
-        //   IP = "192.168.4.1"; //https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-class.html#:~:text=The%20network%20established%20by%20softAP,has%20only%20one%20hardware%20channel.
-        //   port = 111;
-        // }
-        //
-        // // code from https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient?view=net-5.0#constructors
-        // public void establishConnection() {
-        //     upd = new UdpClient(port);
-        //     TcpListener tcp = new TcpListener(IPAddress.Parse(IP), port);
-        //     udp.Connect(IP, port);
-        //
-        //     Byte[] sendBytes = Encoding.ASCII.GetBytes("kobe 8");
-        //
-        //     udp.Send(sendBytes, sendBytes.Length);
-        //
-        //     //IPEndPoint object will allow us to read datagrams sent from any source.
-        //     IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
-        //
-        //     // Blocks until a message returns on this socket from a remote host.
-        //     Byte[] receiveBytes = udp.Receive(ref RemoteIpEndPoint);
-        //     string returnData = Encoding.ASCII.GetString(receiveBytes);
-        //
-        //     // Uses the IPEndPoint object to determine which of these two hosts responded.
-        //     Debug.Log("This is the message you received " +
-        //                               returnData.ToString());
-        //     Debug.Log("This message was sent from " +
-        //                              RemoteIpEndPoint.Address.ToString() +
-        //                              " on their port number " +
-        //                              RemoteIpEndPoint.Port.ToString());
-        //
-        //     udp.Close();
-        // }
-        //
-        // /*
-        // * main method that sends the Data
-        // */
-        // public int sendData() {
-        //
-        //   return 27;
-        // }
 
     }
 }
--- UnityKobe/Assets/Scripts/HeadsetControlsManager.cs
+++ UnityKobe/Assets/Scripts/HeadsetControlsManager.cs
@@ -10,6 +10,11 @@ public class HeadsetControlsManager : MonoBehaviour
 {
   public string textValue;
   public Text textElement;
+
+    // data we are collecting
+    private string RControllerPos;
+
+
   // public Transform trackingSpace;
 
   //public Controller rightController;
@@ -24,28 +29,32 @@ public class HeadsetControlsManager : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-      textElement.text = getData();
+    textElement.text = getData();
+    //updateJSON();
   }
 
   private string getData() {
-    // note: https://docs.unity3d.com/2019.2/Documentation/Manual/OculusControllers.html <-- name of diff parts of controller
-    string data = "<u> Right controller <u>";
-    data += "\n Position: " + OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch).ToString("F3");
-    data += "\n A button press: " + OVRInput.Get(OVRInput.Button.One).ToString();
-    data += "\n B button press: " + OVRInput.Get(OVRInput.Button.Two).ToString();
-    data += "\n ThumbstickPos: " + OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).ToString("F3");
-    data += "\n Thumbstick up: " + OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp).ToString();
-    data += "\n Index trigger: " + OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger).ToString();
-    data += "\n Hand trigger: " + OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger).ToString();
-    data += "\n Thumb rest: " + OVRInput.Get(OVRInput.Touch.SecondaryThumbRest).ToString();
-    data += "\n Haptic feedback: " ;
-    // Debug.Log(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch));
-
-    // let see if this works :/
-    CommsManager comm = new CommsManager();
-    comm.SendMessage();
+        // note: https://docs.unity3d.com/2019.2/Documentation/Manual/OculusControllers.html <-- name of diff parts of controller
+        string data = "<u> Right controller <u>";
+        data += "\n Position: " + OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch).ToString("F3");
+        //RControllerPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch).ToString("F3");
+        data += "\n A button press: " + OVRInput.Get(OVRInput.Button.One).ToString();
+        data += "\n B button press: " + OVRInput.Get(OVRInput.Button.Two).ToString();
+        data += "\n ThumbstickPos: " + OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).ToString("F3");
+        data += "\n Thumbstick up: " + OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp).ToString();
+        data += "\n Index trigger: " + OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger).ToString();
+        data += "\n Hand trigger: " + OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger).ToString();
+        data += "\n Thumb rest: " + OVRInput.Get(OVRInput.Touch.SecondaryThumbRest).ToString();
+        data += "\n Haptic feedback: " ;
+        // Debug.Log(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch));
 
 
-    return data;
+        //string json = JsonUtility.ToJson();
+        // let see if this works :/
+        //CommsManager comm = new CommsManager();
+
+        return data;
   }
+ 
+
 }
