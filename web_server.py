dfe6a40ed060b9eacb36265d3ef83a426a7719e8
alexisdumon27 <52036219+alexisdumon27@users.noreply.github.com>
2021-08-18 17:37:15 +0100

    resolves main conflicts

--- web_server.py
+++ web_server.py
@@ -2,18 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 json = ''
 
 ser = serial.Serial(port = "/dev/ttyACM0", baudrate = 9600, timeout = 1.0)
-=======
-test = ''
->>>>>>> Kobe-68_ChansisBaseBranch
 
 print("script was run")
 
 # receiving a post request
-<<<<<<< HEAD
 @app.route('/', methods=['POST', 'GET'])
 def changeIndex():
     if request.method == 'POST':
@@ -26,19 +21,6 @@ def changeIndex():
         print ("nothing was sent")
         return json
     return json
-=======
-@app.route('/', methods=['POST', 'GET']) 
-def changeIndex():
-    if request.method == 'POST':
-        global test 
-        test = request.get_json()
-        print ("received POST request")
-        print ("test: ", test)
-    else :
-        print ("nothing was sent")
-        return test
-    return test
->>>>>>> Kobe-68_ChansisBaseBranch
 
 # test if we send data
 @app.route('/test')
@@ -48,8 +30,4 @@ def getRequest():
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host = '0.0.0.0', port = 5000, debug=True)
-=======
-    app.run(host = '0.0.0.0', port = 5000, debug=True)
->>>>>>> Kobe-68_ChansisBaseBranch
