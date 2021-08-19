c1e636bbc955171c0819234c0d1b00e6e3046a64
mcd005 <j.pmcdonald005@gmail.com>
2021-08-19 23:33:15 +0100

    Add delay parameter

--- ArduinoKobe/ArduinoFirmwareVSCode/src/Joint.cpp
+++ ArduinoKobe/ArduinoFirmwareVSCode/src/Joint.cpp
@@ -1,13 +1,14 @@
+#include "Joint.hpp"
 #include <Adafruit_PWMServoDriver.h>
 #include <Arduino.h>
-#include "Joint.hpp"
 
 #define MIN_PULSE_WIDTH 150
-#define MAX_PULSE_WIDTH 500
+#define MAX_PULSE_WIDTH 450
 #define FREQUENCY 50
 #define PULSE_ON 15
+#define INCREMENT_DELAY_USECS 0
 
-Joint::Joint(String name, uint16_t startAngle, uint16_t channel, Adafruit_PWMServoDriver* pwmObject) :
+Joint::Joint(String name, uint16_t startAngle, uint16_t channel, Adafruit_PWMServoDriver *pwmObject) :
     jointName(name),
     currentAngle(startAngle),
     targetAngle(startAngle),
@@ -21,7 +22,7 @@ String Joint::setTargetAngle(int givenAngle)
     targetAngle = givenAngle;
     return jointName + " target angle set to " + String(targetAngle);
 }
-    
+
 void Joint::incrementPosition()
 {
     if (currentAngle != targetAngle)
@@ -29,10 +30,11 @@ void Joint::incrementPosition()
         if (currentAngle < targetAngle) ++currentAngle;
         if (currentAngle > targetAngle) --currentAngle;
         pwm->setPWM(jointChannel, PULSE_ON, calculatePulseWidth(currentAngle));
+        delay(INCREMENT_DELAY_USECS);
     }
 }
 
 int Joint::calculatePulseWidth(uint16_t angle)
 {
-    return map(angle, 0, 180, MIN_PULSE_WIDTH , MAX_PULSE_WIDTH);
+    return map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
 }
